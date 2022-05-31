bff123153d603cd4878bbfc9ea6ee9f5e796b846
selymanrastani <selyman.rastani@gmail.com>
2022-05-31 19:54:27 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -3,7 +3,11 @@ package github;
 public class gitAndGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Today we learn github");
+		System.out.println("Today is a very good day");
+
+
 
 	}
 
